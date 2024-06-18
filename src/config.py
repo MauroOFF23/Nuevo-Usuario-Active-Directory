239c20b143d251efa512ab955738b45de9121be5
Mauro Burgos <120226549+MauroOFF23@users.noreply.github.com>
2024-06-17 20:38:42 -0600

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@
 
 import os
 
-LDAP_ADDRESS = os.getenv('LDAP_ADDRESS', '192.168.56.11') #Ingresa dirección ip del servidor, o su nombre de host si se cuenta con DNS
-LDAP_USERNAME = os.getenv('LDAP_USERNAME', 'Administrator@pod3test.local') #El usuario Administrator es usado por fines de laboratorio, cambiar por uno con permisos necesarios. Agregando el dominio 
-LDAP_PASSWORD = os.getenv('LDAP_PASSWORD', 'Cisco123')#Contraseña del usuario
+LDAP_ADDRESS = os.getenv('LDAP_ADDRESS', '192.168.1.100') #Ingresa dirección ip del servidor, o su nombre de host si se cuenta con DNS
+LDAP_USERNAME = os.getenv('LDAP_USERNAME', 'Administrator@pod10.local') #El usuario Administrator es usado por fines de laboratorio, cambiar por uno con permisos necesarios. Agregando el dominio 
+LDAP_PASSWORD = os.getenv('LDAP_PASSWORD', 'Contraseña')#Contraseña del usuario
